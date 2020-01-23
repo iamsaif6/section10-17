b47ad351f8e371c9941b22d91b511535c2a925d8
Saif Hasan <saif@gmail.com>
2020-01-23 13:53:27 +0600

    master

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+     $(".product-list").masonry();
+     $(".owl-slider").owlCarousel({
+          items: 1,
+          loop: true,
+          nav: false,
+          dots : true
+     });
+
+     
+
+
+});
